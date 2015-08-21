1691b4c6344e1eb02476136728876b2752049509
Sharanabasappa Revadigar <revadigar@gmail.com>
2015-08-21 14:31:58 -0700

    Fixed merge conflicts.

--- server/server.js
+++ server/server.js
@@ -77,33 +77,7 @@ app.post('/setprofile', function(req, res) {
     });
 });
 
-// app.post('/organizeLunchFake', function(req, res) {
-//   // store user profile in the db
-//   var dummyResults = [
-//     {
-//       restaurantName: 'The House',
-//       url: 'www.a.com',
-//       location: 'san francisco',
-//       image_url: '<img alt="The House" class="photo-box-img" height="90" src="//s3-media3.fl.yelpcdn.com/bphoto/zRQX2YrSaLA3WLocvl8x8A/90s.jpg" width="90">',
-//       desc: 'The food is amazing with the wasabi noodles topping the chart. &nbsp;• &nbsp;As for appetizers, you have to try the deep fried salmon rolls! The grilled sea bass with garlic ginger soy is also',
-//       address: '1230 Grant Ave, San Francisco, CA 94133',
-//       phone: '(415) 986-8612'
-//     },
-//     {
-//       restaurantName: 'Olive Garden',
-//       url: 'www.olivegarden.com',
-//       location: 'san francisco',
-//       image_url: '<img alt="The House" class="photo-box-img" height="90" src="//s3-media3.fl.yelpcdn.com/bphoto/zRQX2YrSaLA3WLocvl8x8A/90s.jpg" width="90">',
-//       desc: 'The food is amazing with the wasabi noodles topping the chart. &nbsp;• &nbsp;As for appetizers, you have to try the deep fried salmon rolls! The grilled sea bass with garlic ginger soy is also',
-//       address: '1230 Grant Ave, San Francisco, CA 94133',
-//       phone: '(415) 986-8612'
-//     }
-//   ];
-//   res.send(dummyResults);
-// });
-
 app.post('/organizeLunch', function(req, res) {
-<<<<<<< HEAD
   // store user profile in the db
   var dummyResults = [
     {
@@ -135,16 +109,15 @@ app.post('/organizeLunch', function(req, res) {
     }
   ];
 
-  res.send(dummyResults);
-=======
   var userList = req.body;
   console.log("userList", userList);
 
   yelper.findLunch(userList)
-  .then(function(results) {
-    res.send(results);
-  });
->>>>>>> 51fe8a5c669a0bae0dfd94a0811ba6cfbfe6e592
+    .then(function(results) {
+      res.send(results);
+    });
+
+  res.send(dummyResults);
 });
 
 app.get('/users', function(req, res) {
