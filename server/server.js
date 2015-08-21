bb71e3f25e4bcf26b25171234b883ec26142b0d7
Sharanabasappa Revadigar <revadigar@gmail.com>
2015-08-21 12:43:11 -0700

    Add some dummy results

--- server/server.js
+++ server/server.js
@@ -74,7 +74,27 @@ app.post('/setprofile', function(req, res) {
 
 app.post('/organizeLunch', function(req, res) {
   // store user profile in the db
-  res.send('OK');
+  var dummyResults = [
+    {
+      restaurantName: 'The House',
+      url: 'www.a.com',
+      location: 'san francisco',
+      image_url: '<img alt="The House" class="photo-box-img" height="90" src="//s3-media3.fl.yelpcdn.com/bphoto/zRQX2YrSaLA3WLocvl8x8A/90s.jpg" width="90">',
+      desc: 'The food is amazing with the wasabi noodles topping the chart. &nbsp;• &nbsp;As for appetizers, you have to try the deep fried salmon rolls! The grilled sea bass with garlic ginger soy is also',
+      address: '1230 Grant Ave, San Francisco, CA 94133',
+      phone: '(415) 986-8612'
+    },
+    {
+      restaurantName: 'Olive Garden',
+      url: 'www.olivegarden.com',
+      location: 'san francisco',
+      image_url: '<img alt="The House" class="photo-box-img" height="90" src="//s3-media3.fl.yelpcdn.com/bphoto/zRQX2YrSaLA3WLocvl8x8A/90s.jpg" width="90">',
+      desc: 'The food is amazing with the wasabi noodles topping the chart. &nbsp;• &nbsp;As for appetizers, you have to try the deep fried salmon rolls! The grilled sea bass with garlic ginger soy is also',
+      address: '1230 Grant Ave, San Francisco, CA 94133',
+      phone: '(415) 986-8612'
+    }
+  ];
+  res.send(dummyResults);
 });
 
 app.get('/users', function(req, res) {
